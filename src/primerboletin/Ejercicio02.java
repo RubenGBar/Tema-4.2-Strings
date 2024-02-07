29b263924c0fa9bf86911e654109410f12163bfb
RubenGBar <ruben.garcia@iesnervion.es>
2024-02-07 09:41:57 +0100

    02 terminado

---
+++ src/primerboletin/Ejercicio02.java
@@ -0,0 +1,13 @@
+package primerboletin;
+
+import java.util.Scanner;
+
+public class Ejercicio02 {
+	public static void main(String[] args) {
+	// Creo un objeto de la clase Scanner 
+	Scanner sc = new Scanner(System.in);
+	
+	// Cierro el Scanner
+	sc.close();
+	}
+}
