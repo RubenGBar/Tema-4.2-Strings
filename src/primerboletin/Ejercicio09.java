2109a78317291c78b947bae2ea49730379410763
RubenGBar <ruben.garcia@iesnervion.es>
2024-02-15 12:46:42 +0100

    09 sin terminar

---
+++ src/primerboletin/Ejercicio09.java
@@ -0,0 +1,22 @@
+package primerboletin;
+
+import java.util.Scanner;
+
+public class Ejercicio09 {
+	public static void main(String[] args) {
+		// Creo el Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Cierro el Scanner
+		sc.close();
+	}
+	
+	public static void traductor(String frase) {
+		// Variable para no mo
+		
+		/*
+		 * NO HAY QUE REMPLAZAR
+		 * */
+	}
+
+}
