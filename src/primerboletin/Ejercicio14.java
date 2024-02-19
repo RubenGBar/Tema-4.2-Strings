9d7dd06ec1d4c3de70cd679c43811879cc8b9d8e
ruben <ruben@DESKTOP-QTJ799Q>
2024-02-19 19:28:15 +0100

    no tengo ni puta idea

---
+++ src/primerboletin/Ejercicio14.java
@@ -0,0 +1,17 @@
+package primerboletin;
+
+import java.util.Scanner;
+
+public class Ejercicio14 {
+	public static void main(String[] args) {
+		// Creo el Scanner 
+		Scanner sc = new Scanner(System.in);
+		// Variable para guardar la frase introducida por teclado y contar sus letras
+		String frase = "";
+		
+		
+		// Cierro el Scanner
+		sc.close();
+	}
+
+}
