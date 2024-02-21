00c8d6fcf84f1c1e39e52f6e901694c54a943b9f
RubenGBar <ruben.garcia@iesnervion.es>
2024-02-21 10:11:17 +0100

    19 terminado

--- src/primerboletin/Ejercicio19.java
+++ src/primerboletin/Ejercicio19.java
@@ -32,30 +32,34 @@ public class Ejercicio19 {
 	 * @return Una cadena que es un nombre para una variable en Java
 	 */
 	public static String aVariableJava(String frase) {
-		// Variable para guardar la frase introducida como el nombre de una variable en Java
-		String fraseEnVariable = "";
-		// Variable para guardar una copia de la frase
-		String copiaFrase = frase;
 		// Variable para guardar una copia de la frase en minúsculas
-		String fraseMin = copiaFrase.toLowerCase();		
-		// Variable para guardar una copia de la frase en mayúsculas
-		String fraseMayus = copiaFrase.toUpperCase();
+		String fraseMin = frase.toLowerCase();		
+		// Variable para construir la nueva cadena en notación Camel
+		String fraseVariable = "";
+		// Array para dividir la cadena recibida
+		String[] fraseEnPalabras = fraseMin.split(" ");
+		// Variable para obtener la palbra que se guarda en cada posición del Array
+		String palabra = "";
+		// Variable para guardar el caracter a cambiar
+		char caracterACambiar = ' ';
 		
 		// Bucle for para convertir la cadena en el nombre de una variable en Java
-		for(int i = 0; i < copiaFrase.length(); i++) {
-			// Compruebo que el caracter en esa iteración sea un espacio
-			if(copiaFrase.charAt(i) == ' ') {
-				// Cambio el caracter después del espacio por el mismo, pero en mayíusculas
-				fraseMin = fraseMin.replace(copiaFrase.charAt(i+1), fraseMayus.charAt(i+1));
-			}
+		for(int i = 0; i < fraseEnPalabras.length; i++) {
+			// Me quedo con la palabra de la posición de esa iteración del bucle
+			palabra = fraseEnPalabras[i];
+			// Me quedo con el caracter del principio de cada palabra
+			caracterACambiar = palabra.charAt(0);
 			
+			// Compruebo que la i no sea 0 para no cambiar la primera palabra
+			if(i != 0) {
+				// Cambio el primer caracter a mayúculas y lo concadeno con el resto de la frase
+				palabra = Character.toUpperCase(caracterACambiar) + palabra.substring(1);
+			}
+			// Construyo con las palabras modificadas la cadena en notación camel
+			fraseVariable += palabra;
 		}
-		
-		// Elimino los espacios
-		fraseEnVariable = fraseMin.replace(" ", "");
-		
 		// Devuelvo fraseEnVariable como valor de la función 
-		return fraseEnVariable;
+		return fraseVariable;
 	}
 
 }
